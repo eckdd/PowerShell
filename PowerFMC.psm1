3af3d9f75cf40c94b238f8e99ce8c65ce7c38510
eckdd <decker.dl@gmail.com>
2018-09-29 20:28:21 -1000

    Update PowerFMC.psm1

--- PowerFMC.psm1
+++ PowerFMC.psm1
@@ -1,15 +1,16 @@
-
-
-function Get-FMCNetworkObjects {
+#############################################################################################################
+#############################################################################################################
+#############################################################################################################
+function Get-FMCAuthToken {
 <#
  .SYNOPSIS
-Displays network objects in FMC
+Obtains Domain UUID and X-auth-access-token
  .DESCRIPTION
-This cmdlet will invoke a REST request against the FMC API and retrieve items under /object/networks
+This cmdlet will invoke a REST post against the FMC API, authenticate, and provide an X-auth-access-token and
+Domain UUID for use in other functions
 
-This cmdlet can operate on several phoens at once.
  .EXAMPLE
-# Get-FMCNetworkObjects -fmcHost "https://fmcrestapisandbox.cisco.com" -username 'davdecke' -password $password
+# Get-FMCNetworkObjects -fmcHost "https://fmcrestapisandbox.cisco.com" -username 'davdecke' -password 'xxxxxx'
 
  .PARAMETER fmcHost
 Base URL of FMC
@@ -20,18 +21,15 @@ REST account password
 
 /#>
 
-
     param
     (
         [Parameter(Mandatory=$true,
         ValueFromPipeline=$true,
         ValueFromPipelineByPropertyName=$true)]
             [string]$fmcHost,
-        [Parameter(Mandatory=$false,
-        ValueFromPipeline=$true,
-        ValueFromPipelineByPropertyName=$true)]
             [string]$username,
             [string]$password
+
     )
 
 add-type @"
@@ -46,30 +44,83 @@ add-type @"
     }
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 
 $credPair = "$($username):$($password)"
 $encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credPair))
 $uri = "$fmcHost/api/fmc_platform/v1/auth/generatetoken"
 $headers = @{ Authorization = "Basic $encodedCredentials" }
-[System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 
 $AuthResponse = Invoke-WebRequest -Uri $uri -Headers $headers -Method Post
 
 $DOMAIN_UUID =  $AuthResponse.Headers.Item('DOMAIN_UUID')
 $AuthAccessToken = $AuthResponse.Headers.Item('X-auth-access-token')
+$output = New-Object -TypeName psobject
+$output | Add-Member -MemberType NoteProperty -Name Domain -Value $DOMAIN_UUID
+$output | Add-Member -MemberType NoteProperty -Name 'AuthAccessToken' -Value $AuthAccessToken
+$output
+}
 
-$uri = "$fmcHost/api/fmc_config/v1/domain/$DOMAIN_UUID/object/networks"
+#############################################################################################################
+#############################################################################################################
+#############################################################################################################
+
+function Get-FMCNetworkObjects {
+<#
+ .SYNOPSIS
+Displays network objects in FMC
+ .DESCRIPTION
+This cmdlet will invoke a REST request against the FMC API and retrieve items under /object/networks
+ .EXAMPLE
+# Get-FMCNetworkObjects -fmcHost "https://fmcrestapisandbox.cisco.com" -username 'davdecke' -password 'xxxxxx'
+
+ .PARAMETER fmcHost
+Base URL of FMC
+ .PARAMETER AuthAccessToken
+X-auth-accesss-token 
+ .PARAMETER Domain
+Domain UUID 
+/#>
+
+    param
+    (
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true)]
+            [string]$fmcHost,
+        [Parameter(Mandatory=$false,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true)]
+            [string]$AuthAccessToken,
+            [string]$Domain
+    )
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
+$uri = "$fmcHost/api/fmc_config/v1/domain/$Domain/object/networks"
 $headers = @{ "X-auth-access-token" = "$AuthAccessToken" }
-
 $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers
 $NetObjects = @()
 $response.items.links.self | foreach {
     $NetObjects += Invoke-RestMethod -Method Get -Uri $_ -Headers $headers
 
         }
-$NetObjects | select type,name,value,description | fl
-
+$NetObjects | select type,name,value,description
 }
+
+#############################################################################################################
+#############################################################################################################
+#############################################################################################################
 
 function New-FMCNetworkObject {
 <#
@@ -77,17 +128,15 @@ function New-FMCNetworkObject {
 Create network objects in FMC
  .DESCRIPTION
 This cmdlet will invoke a REST request against the FMC API and retrieve items under /object/networks
-
-This cmdlet can operate on several phoens at once.
  .EXAMPLE
 # New-FMCNetworkObject -fmcHost $fmcHost -username davdecke -password $password -name 'PowerFMC_172.21.33.0/24' -Network "172.21.33.0" -Prefix 24 -description "Test Object for PowerFMC 2"
 
  .PARAMETER fmcHost
 Base URL of FMC
- .PARAMETER username
-REST account username
- .PARAMETER password
-REST account password
+ .PARAMETER AuthAccessToken
+X-auth-accesss-token 
+ .PARAMETER Domain
+Domain UUID 
  .PARAMETER name
 Name of the rule. Illegal characters (/,\,whitespaces) are automatically replaced with underscrores 
  .PARAMETER Network
@@ -95,7 +144,6 @@ The network or host dotted-decimal IP
  .PARAMETER Prefix
 Prefix length for network (32 for host)
 /#>
-
 
     param
     (
@@ -110,11 +158,12 @@ Prefix length for network (32 for host)
         [Parameter(Mandatory=$false,
         ValueFromPipeline=$true,
         ValueFromPipelineByPropertyName=$true)]
-            [string]$username,
-            [string]$password,
             [string]$description,
             [string]$overridable="false",
-            [string]$type="network"
+            [string]$type="network",
+            [string]$AuthAccessToken,
+            [string]$Domain
+
     )
 
 add-type @"
@@ -129,19 +178,8 @@ add-type @"
     }
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-
-$credPair = "$($username):$($password)"
-$encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credPair))
-$uri = "$fmcHost/api/fmc_platform/v1/auth/generatetoken"
-$headers = @{ Authorization = "Basic $encodedCredentials" }
 [System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
-
-$AuthResponse = Invoke-WebRequest -Uri $uri -Headers $headers -Method Post
-
-$DOMAIN_UUID =  $AuthResponse.Headers.Item('DOMAIN_UUID')
-$AuthAccessToken = $AuthResponse.Headers.Item('X-auth-access-token')
-
-$uri = "$fmcHost/api/fmc_config/v1/domain/$DOMAIN_UUID/object/networks"
+$uri = "$fmcHost/api/fmc_config/v1/domain/$Domain/object/networks"
 $headers = @{ "X-auth-access-token" = "$AuthAccessToken" ;'Content-Type' = 'application/json' }
 
 $value = "$Network/$Prefix"
@@ -157,3 +195,7 @@ $body | Add-Member -MemberType NoteProperty -name type        -Value $type
 $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body ($body | ConvertTo-Json)
 $response
 }
+
+#############################################################################################################
+#############################################################################################################
+#############################################################################################################
